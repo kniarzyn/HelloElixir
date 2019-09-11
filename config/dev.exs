c295e6094c6af1fc888bcf7543e031278547be9f
Paweł Kwietniewski <Pawel.Kwietniewski@grupaazoty.com>
2019-09-11 15:11:25 +0200

    Configure db

--- config/dev.exs
+++ config/dev.exs
@@ -3,7 +3,7 @@ use Mix.Config
 # Configure your database
 config :hello, Hello.Repo,
   username: "postgres",
-  password: "postgres",
+  password: "example",
   database: "hello_dev",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
--- config/test.exs
+++ config/test.exs
@@ -3,7 +3,7 @@ use Mix.Config
 # Configure your database
 config :hello, Hello.Repo,
   username: "postgres",
-  password: "postgres",
+  password: "example",
   database: "hello_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
